20020f36d3be89f905385974693f88b2202e29a7
Mahmoud <mahmoudmagdy20002@gmail.com>
2023-06-18 17:52:01 +0300

    ddd

--- session_3/lib/session_3.dart
+++ session_3/lib/session_3.dart
@@ -1,14 +1,7 @@
 
 import 'dart:io';
 void main(){
-  int factorial=1;
-  int sum=0;
-for(int i=1;i<=4;i++){
-  factorial=factorial*i;
-  sum+=i;
-}
-print(factorial);
-print(sum);
+ print("hello git");
 
 
 
