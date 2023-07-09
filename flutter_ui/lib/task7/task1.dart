c99476738cc2232a510ae9acd0c39758175e9ef4
Mahmoud <mahmoudmagdy20002@gmail.com>
2023-07-10 00:17:34 +0300

    task7

---
+++ flutter_ui/lib/task7/task1.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui/onc.dart';
+
+import '../task6/task.dart';
+
+// void main() {
+//   runApp(InteractiveWidgets());
+// }
+
+// ignore: must_be_immutable
+class InteractiveWidgets extends StatelessWidget {
+  InteractiveWidgets({super.key});
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+          child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Up Academy'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                    hintText: 'Email', border: OutlineInputBorder()),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                    hintText: 'Password', border: OutlineInputBorder()),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                    
+                   
+                    if(emailController.text=='flutter@gmail.com'&&passwordController.text=='upacademy'){
+                      return BusinessCard();
+                    }
+                    else{
+                      return Center(child: Text('ERROR'),);
+                    }
+                    
+                  }));
+                },
+              
+                child: const Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
